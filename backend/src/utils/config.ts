fead70668a947876228b007505d977c252e529f7
CristianIgnacio <cristianignacio71@gmail.com>
2026-04-25 03:46:23 -0400

    feat: update configuration for default values and improve readability

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -3,22 +3,26 @@ dotenv.config()
 
 type SameSitePolicy = 'lax' | 'strict' | 'none'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const HOST = process.env.HOST || 'localhost'
 
-const MONGODB_URI
-  = process.env.NODE_ENV === 'test'
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI
+    : process.env.NODE_ENV === 'production'
+      ? process.env.MONGODB_URI
+      : process.env.MONGODB_URI_LOCAL
 
 const JWT_SECRET = process.env.JWT_SECRET
 if (!JWT_SECRET) throw new Error('JWT_SECRET environment variable is required')
-const MONGODB_DBNAME 
+
+  const MONGODB_DBNAME 
   = process.env.NODE_ENV === 'test' 
     ? process.env.TEST_MONGODB_DBNAME 
-    : process.env.MONGODB_DBNAME || 'postsdb'
+    : process.env.MONGODB_DBNAME || 'soloropa'
 
 const SCRAPER_MONGODB_URI = process.env.SCRAPER_MONGODB_URI
+
 const COOKIE_SAME_SITE: SameSitePolicy = (
   process.env.COOKIE_SAME_SITE === 'none'
   || process.env.COOKIE_SAME_SITE === 'strict'
@@ -27,4 +31,12 @@ const COOKIE_SAME_SITE: SameSitePolicy = (
   ? process.env.COOKIE_SAME_SITE
   : 'lax'
 
-export default { PORT, MONGODB_URI, HOST, JWT_SECRET, MONGODB_DBNAME, SCRAPER_MONGODB_URI, COOKIE_SAME_SITE }
+export default { 
+  PORT, 
+  MONGODB_URI, 
+  HOST, 
+  JWT_SECRET, 
+  MONGODB_DBNAME, 
+  SCRAPER_MONGODB_URI, 
+  COOKIE_SAME_SITE 
+}
